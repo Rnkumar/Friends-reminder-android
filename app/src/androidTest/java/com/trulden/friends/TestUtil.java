2860b0919181980a3905f3af58b970fc7f97d9b4
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-01 12:13:33 +0300

    add bg color checker

--- app/src/androidTest/java/com/trulden/friends/TestUtil.java
+++ app/src/androidTest/java/com/trulden/friends/TestUtil.java
@@ -1,9 +1,17 @@
 package com.trulden.friends;
 
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.internal.util.Checks;
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import androidx.test.runner.lifecycle.Stage;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -23,5 +31,19 @@ public class TestUtil {
         });
 
         return currentActivity[0];
+    }
+
+    public static Matcher<View> withBgColor(final int color) {
+        Checks.checkNotNull(color);
+        return new BoundedMatcher<View, RelativeLayout>(RelativeLayout.class) {
+            @Override
+            public boolean matchesSafely(RelativeLayout layout) {
+                return color == ((ColorDrawable) layout.getBackground()).getColor();
+            }
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with text color: ");
+            }
+        };
     }
 }
